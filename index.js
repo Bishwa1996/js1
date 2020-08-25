5af3ab78449a7d2dbf138141429d7d98ea1a603d
iamBishwa <bishwa1996@gmail.com>
2020-08-25 20:46:30 +0800

    adding two files

---
+++ index.js
@@ -0,0 +1,72 @@
+// 1. ways to print in javascript
+// console.log("Hello World");
+// alert("me");
+// document.write("this is document write")
+
+// 2. Javascript console API
+// console.log("Hello World");
+// console.warn("this is warning");
+// console.error("this is an error");
+
+// 3. javascript variables
+//  variables---containers to store data values
+var number1 = 12;
+var number2 = 86;
+// console.log(number1 + number2);
+
+// 4. Data types in js
+// Numbers
+var num1 = 455;
+var num2 = 57.75;
+// strings
+var str1 = "This is a string";
+var str2 = "This is another string";
+// objects
+var marks = {
+    ravi: 34,
+    shubham: 80,
+    ankit: 90,}
+    // console.log(marks);
+// Booleans
+var a = true;
+var b = false;
+// console.log(a,b);
+
+// var und = undefined;
+var und;
+// console.log(und);
+
+var n=null
+// console.log(n);
+
+/*two types of data types:-
+1. Primitive: undefined, null, number, string, boolean, symbol
+2. reference: arrays, objects     */
+
+var arr = [ 1, 2, "string" , 4 ,5 ];
+// console.log(arr);
+
+// Operators types :-
+// 1. Arithmetic operator:-
+var a = 100;
+var b = 10;
+// console.log("The value of a*b is",a*b);
+
+// 2. Assignment operator:-
+var c=b;
+c +=2;
+// console.log(c);
+
+// 3. Comparison operator:-
+var x =17;
+var y =19;
+// console.log(x==y);
+// console.log(x<=y);
+
+// 4. logical operator:-
+/*console.log(true && true);
+console.log(true || false);
+console.log(false && false);*/
+// logical not
+/*console.log(!false);
+console.log(!true)*/
