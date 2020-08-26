3f057d332e9b35879b03b27df2adf8c3f9b611b3
iamBishwa <bishwa1996@gmail.com>
2020-08-26 10:46:07 +0800

    modfied index file

--- index.js
+++ index.js
@@ -1,72 +1,46 @@
-// 1. ways to print in javascript
-// console.log("Hello World");
-// alert("me");
-// document.write("this is document write")
 
-// 2. Javascript console API
-// console.log("Hello World");
-// console.warn("this is warning");
-// console.error("this is an error");
+// function in js:-
 
-// 3. javascript variables
-//  variables---containers to store data values
-var number1 = 12;
-var number2 = 86;
-// console.log(number1 + number2);
-
-// 4. Data types in js
-// Numbers
-var num1 = 455;
-var num2 = 57.75;
-// strings
-var str1 = "This is a string";
-var str2 = "This is another string";
-// objects
-var marks = {
-    ravi: 34,
-    shubham: 80,
-    ankit: 90,}
-    // console.log(marks);
-// Booleans
-var a = true;
-var b = false;
-// console.log(a,b);
-
-// var und = undefined;
-var und;
-// console.log(und);
-
-var n=null
-// console.log(n);
-
-/*two types of data types:-
-1. Primitive: undefined, null, number, string, boolean, symbol
-2. reference: arrays, objects     */
-
-var arr = [ 1, 2, "string" , 4 ,5 ];
-// console.log(arr);
-
-// Operators types :-
-// 1. Arithmetic operator:-
-var a = 100;
-var b = 10;
-// console.log("The value of a*b is",a*b);
-
-// 2. Assignment operator:-
-var c=b;
-c +=2;
+function avg(a, b) {
+    return (a + b) / 2;
+}
+// DRY= do not repeat yourself
+c = avg(4, 6);
 // console.log(c);
 
-// 3. Comparison operator:-
-var x =17;
-var y =19;
-// console.log(x==y);
-// console.log(x<=y);
+// conditionals in js:-
+var age = 34;
+if (age > 18) {
+    // console.log('you can vote');
+}
+else {
+    // console.log('you can not vote');
+}
+var arr = [1, 2, 3, 4, 5, 6, 7];
+// console.log(arr);
+for (var i = 0; i < arr.length; i++) {
+    // console.log(arr[i]);
+}
+//   OR
+arr.forEach(function (element) {
+    // console.log(element);
+})
 
-// 4. logical operator:-
-/*console.log(true && true);
-console.log(true || false);
-console.log(false && false);*/
-// logical not
-/*console.log(!false);
-console.log(!true)*/
+/*let j = 0;
+const ac = 0;
+ac++;*/
+
+let j = 0;
+while (j < arr.length) {
+    // console.log(arr[j]);
+    j++;
+}
+for (var i = 0; i < arr.length; i++){
+    if(i==2){
+        // break;
+        continue;
+    }
+    console.log(arr[i]);
+}
+
+
